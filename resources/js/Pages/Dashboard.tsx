5d4e7a0b0198498015dc68d1e7eeb39f472e3cdc
mgravemeyer <m.gravemeyer@icloud.com>
2024-07-10 23:03:12 +0200

    removed boilerplate, added UI, added data model and migration

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,22 +1,25 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
-import { PageProps } from '@/types';
+type DashboardProps = {
+    data: any[]
+}
 
-export default function Dashboard({ auth }: PageProps) {
+export default function Dashboard({data}: DashboardProps) {
+
+    console.log(data)
+
     return (
-        <AuthenticatedLayout
-            user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
-        >
-            <Head title="Dashboard" />
-
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
-                    </div>
+        <div className="w-screen my-20 flex flex-col justify-center items-center bg-gray-100">
+            <div className='w-96 flex flex-col h-fit bg-gray-200'>
+                <div className="flex justify-between bg-gray-700 rounded-tl-md rounded-tr-md py-2 px-6">
+                    <p className="text-white text-lg">Zeit</p>
+                    <p className="ml-8 text-white text-lg">Daten</p>
                 </div>
+                {data.map((element, index) => {
+                    return <div className={`flex justify-between px-4 py-1 ${index === 0 ? 'pt-3' : ''} ${index === data.length - 1 ? 'pb-3' : ''}`}>
+                        <p className="text-gray-700 text-lg">{element.time}</p>
+                        <p className="ml-8 text-gray-700 text-lg">{element.value}</p>
+                    </div>
+                })}
             </div>
-        </AuthenticatedLayout>
-    );
+        </div>
+    )
 }
--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,22 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Models\Data;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
+    return Inertia::render('Dashboard', [
+        'data' => Data::all(),
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::post('/api/v1/data', function () {
+    $data = new Data();
+    $data->value = request()->input('value');
+    $data->time = request()->input('time');
+    $data->save();
+    return response()->json(['message' => 'created', 'data' =>$data], 200);
 });
 
 require __DIR__.'/auth.php';
